a9ffbbe4f0d14b0e56f82ac862636f403746431c
Josepe <josepedrozo99@gmail.com>
2025-09-02 23:50:05 -0300

    chore: update Tailwind CSS configuration and dependencies
    - Updated Tailwind CSS to version 3.3.3 and PostCSS to version 8.5.6 in package.json.
    - Added autoprefixer as a dependency.
    - Created a new postcss.config.js file to configure Tailwind CSS and autoprefixer.
    - Removed the old postcss.config.mjs file.
    - Cleaned up globals.css by removing unnecessary comment.
    - Updated tailwind.config.ts to reflect new content paths for Tailwind CSS.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
-// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{tsx}",
+    "./src/components/**/*.{tsx}",
   ],
   theme: {
     extend: {
